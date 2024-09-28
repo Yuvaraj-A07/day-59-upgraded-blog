00d0aa6dafe3d2bd682561b45605417eb2191952
Yuvaraj-A07 <yuvaraj07at@gmail.com>
2024-09-28 19:13:00 +0530

    blog upgrade

---
+++ main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template
+import requests
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+# posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+posts = requests.get("https://www.npoint.io/docs/9081394ea17546f64875").json() # This link is not working so I need
+# to create a new npoint
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5001)
